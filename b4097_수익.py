48aebc4f84c500bf2b5e92f2892c3abc8ac0a339
Min Seo Shin <forthesouldance@gmail.com>
2022-09-12 18:01:13 +0900

    오랜만이쥬~

--- b4097_수익.py
+++ b4097_수익.py
@@ -1,15 +1,13 @@
-while True:
-    n = int(input())
+while True: #n이 0일 때까지 반복이므로 while True문으로 작성 (무한 루프
+    n = int(input()) #n 입력받음
     arr = []
     for i in range(n):
         arr.append(int(input()))
-    if n == 0:
+    if n == 0: #n이 0이면 while문 빠져나오기
         break
-    elif n == 1:
-        print(arr[0])
     else:
-        D = [0 for _ in range(len(arr))]
+        D = [0 for _ in range(len(arr))] #배열 D 만들어주기
         D[0] = arr[0]
-        for i in range(1, len(arr)):
-            D[i] = max(D[i-1]+arr[i], arr[i])
+        for i in range(1, len(arr)): # i -> 1부터 arr을 길이까지 for문 돌려주기
+            D[i] = max(D[i-1]+arr[i], arr[i]) #점화식
     print(max(D))
